2599607aeb65b542fc3a56db357e7011e79c30ee
Fabricio Bedoya <fabricio.bedoya@edelman.com>
2026-03-04 15:45:25 -0500

    fixing all entries and apply to serve by uuid

--- controller/video/stream.js
+++ controller/video/stream.js
@@ -8,6 +8,62 @@ class VideoStream {
         this.app = null;
         VideoOrm.setUserServiceRole()
     }
+    async retrieveVideoFile(uuid) {
+        return new Promise(async (resolve, reject) => {
+            try {
+                await VideoOrm.retrieveVideoByUuid(uuid)
+            }
+        });
+    }
+    async stream(req, res) {
+        const { uuid} = req.params;
+        const { range } = req.query;
+        if (!uuid || !key) {
+            res.status(400);
+            res.send({message: "Missing arguments in request"});
+            return res;
+        }
+        const videoDetail = await VideoOrm.retrieveVideoDetailByVideoUuid(uuid);
+        try {
+            let videoPath = videoDetail.file;
+            if (!fs.existsSync(videoPath)) {
+                res.status(500);
+                res.send({message: "Missing arguments in request"});
+                return res;
+            }
+            const fileSize = videoDetail.size;
+            const range = req.headers.range;
+            if (range) {
+                const parts = range.replace(/bytes=/, '').split('-');
+                const start = parseInt(parts[0], 10);
+                const end = parts[1] ? parseInt(parts[1], 10) : fileSize - 1;
+                const chunkSize = end - start + 1;
+                const file = fs.createReadStream(videoPath, { start, end });
+                const headers = {
+                    'Content-Range': `bytes ${start}-${end}/${fileSize}`,
+                    'Accept-Ranges': 'bytes',
+                    'Content-Length': chunkSize,
+                    'Content-Type': 'video/mp4',
+                };
+
+                res.writeHead(206, headers);
+                file.pipe(res);
+                return res;
+            } else {
+                const headers = {
+                    'Content-Length': fileSize,
+                    'Content-Type': 'video/mp4',
+                };
+                res.writeHead(200, headers);
+                fs.createReadStream(videoPath).pipe(res);
+                return res;
+            }
+        } catch (err) {
+            res.status(404);
+            res.send(err);
+            return res;
+        }
+    }
     async download(req, res) {
         const { uuid, key} = req.params;
         const { range } = req.query;
